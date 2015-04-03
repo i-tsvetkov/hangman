73e2f9268df9d8a0ceba27c696ba51e4392b2907
i-tsvetkov <i.tsvetkov.4@gmail.com>
2015-04-03 18:21:25 +0300

    corrected extensions

--- mk-cache.rb
+++ mk-cache.rb
@@ -2,9 +2,9 @@ require './hangman-common.rb'
 
 load_words_tree
 
-File.write("./words_tree_cache.json", $words.to_s)
+File.write("./words_tree_cache.rb", $words.to_s)
 
 load_words
 
-File.write("./words_cache.json", $words.to_s)
+File.write("./words_cache.rb", $words.to_s)
 
