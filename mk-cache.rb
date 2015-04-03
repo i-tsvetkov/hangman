15fe0b4cb1e3570f05ab09cdf017b905e2cd0034
i-tsvetkov <i.tsvetkov.4@gmail.com>
2015-04-03 18:10:42 +0300

    JSON is replaced with eval

--- mk-cache.rb
+++ mk-cache.rb
@@ -1,11 +1,10 @@
-require 'json'
 require './hangman-common.rb'
 
 load_words_tree
 
-File.write("./words_tree_cache.json", JSON.generate($words))
+File.write("./words_tree_cache.json", $words.to_s)
 
 load_words
 
-File.write("./words_cache.json", JSON.generate($words))
+File.write("./words_cache.json", $words.to_s)
 
